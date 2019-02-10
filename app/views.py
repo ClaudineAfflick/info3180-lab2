f309d8fddf5fbccbd672df920cf47c2b73047234
ClaudineAfflick <claudine.afflick@mymona.uwi.edu>
2019-02-10 20:28:21 +0000

    info3180-lab2(update)

--- app/views.py
+++ app/views.py
@@ -4,7 +4,7 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
+import datetime
 from app import app
 from flask import render_template, request, redirect, url_for, flash
 
@@ -18,12 +18,15 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
-
 @app.route('/about/')
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/profile/')
+def profile():
+    """Render the profile page."""
+    return render_template('profile.html',name="Claudine Afflick",username="CAA97",location="Kingston,Jamaica",date="February, 2019" )
 
 ###
 # The functions below should be applicable to all Flask apps.
@@ -34,8 +37,17 @@ def send_text_file(file_name):
     """Send your static text file."""
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
+    
 
-
+# def format_date_joined(date):
+#     now = datetime.datetime.now() # today's date
+#     date_joined = datetime.date(2019, 2, 7) # a specific date
+#     ## Format the date to return only month and year date
+    
+#     print date_joined.strftime("%B, %Y")
+    
+    
+    
 @app.after_request
 def add_header(response):
     """
